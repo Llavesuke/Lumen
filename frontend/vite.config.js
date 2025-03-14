5543c32a2d3708570ddd1d9191a3a1152bc97eb2
Llavesuke <llavesukeprogram@gmail.com>
2025-03-14 21:09:42 +0100

    Prueba

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: '.', // Explicitly define the root directory
   plugins: [
     vue(),
     vueDevTools(),
@@ -14,5 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
